8b089d11b43f3ad0a0f1e6ddb8cdef00039ec554
Spencer Olson <spence1818@gmail.com>
2014-08-27 18:22:03 -0500

    Modify record

--- record.rb
+++ record.rb
@@ -1,13 +1,16 @@
+require 'Date'
+
 class Record
+	attr_reader :last_name, :gender, :date_of_birth
 	def initialize args
 		@first_name = args[:first_name].capitalize
 		@last_name = args[:last_name].capitalize
 		@gender = args[:gender].upcase
 		@favorite_color = args[:favorite_color].downcase
-		@date_of_birth = args[:date_of_birth]
+		@date_of_birth = DateTime.strptime(args[:date_of_birth], '%m/%d/%Y')
 	end
 
 	def to_s
-		"#{@last_name} #{@first_name} #{@gender} #{@favorite_color} #{@date_of_birth}"
+		"#{@last_name} #{@first_name} #{@gender} #{@favorite_color} #{@date_of_birth.strftime('%m/%d/%Y')}"
 	end
 end
